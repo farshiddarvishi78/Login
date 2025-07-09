b7a6d13dc824b6e9f170f456547fde3da32ee07d
farshiddarvishi78 <farshid.darvishi99@gmail.com>
2025-07-09 21:24:01 +0330

    login-second-edition

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function(){
+
+    // let user_name = jQuery('#username');
+    // user_name.hide();
+
+    // let username = jQuery('label.input');
+    // username.hide();
+
+    // jQuery('input.submit').click(function(){
+    //     alert('آیا اطلاعات شما درست میباشد؟');
+    // });
+
+    // jQuery('input.password').mouseenter(function(){
+    //     alert('دقت داشته باشید فقط یکبار امکان تایید رمز میباشد بار دوم اکانت شما بن میشود.')
+    // });
+    
+});
